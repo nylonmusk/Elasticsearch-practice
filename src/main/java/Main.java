34723b835c9d46bff8eb3d2c8be03d08987b9498
nylonmusk <2912rla@gmail.com>
2024-03-05 16:53:26 +0900

    feat : 데이터 생성, 조회

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,32 +1,45 @@
-import org.apache.http.HttpHost;
 import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RequestOptions;
-import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestClientBuilder;
-import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        String hostname = "localhost";
-        int port = 9200;
+        try (ElasticConfiguration elasticConfiguration = new ElasticConfiguration("localhost", 9200, "")) {
 
-        HttpHost host = new HttpHost(hostname, port);
-        RestClientBuilder restClientBuilder = RestClient.builder(host);
-        RestHighLevelClient client = new RestHighLevelClient(restClientBuilder);
+            // 데이터 생성 (Index)
+            String index = "indexindex";
+            String id = "1";
+            Map<String, Object> data = new HashMap<>();
+            data.put("name", "John Doe");
+            data.put("age", 30);
 
-        String index = "thisisindex";
-        String id = "2";
-        GetRequest getRequest = new GetRequest(index, id);
-        RequestOptions options = RequestOptions.DEFAULT;
+            IndexRequest indexRequest = new IndexRequest(index).id(id)
+                    .source(data, XContentType.JSON);
 
-        GetResponse response = client.get(getRequest, options);
+            IndexResponse indexResponse = elasticConfiguration.getElasticClient().index(indexRequest, RequestOptions.DEFAULT);
+            System.out.println("Index created: " + indexResponse.getId());
 
-        Map<String, Object> map = response.getSourceAsMap();
-        System.out.println(map);
+            // 데이터 조회 (Get)
+            GetResponse getResponse = elasticConfiguration.getElasticClient().get(new GetRequest(index, id), RequestOptions.DEFAULT);
+            Map<String, Object> retrievedData = getResponse.getSourceAsMap();
+            System.out.println("Retrieved data: " + retrievedData);
+
+            // 데이터 수정 (Update)
+            // (Update operation can be performed using the Update API)
+
+            // 데이터 삭제 (Delete)
+            // (Delete operation can be performed using the Delete API)
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
